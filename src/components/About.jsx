4789b5ec4dcb6a414d22d3c5eb017a3bc5ca19dc
Muhammad Junaid <m.junaidlkl681@gmail.com>
2024-07-16 23:55:59 +0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+
+const About = () => {
+  return (
+    <>
+    <motion.div>
+      <p className={styles.sectiobSubText}>Introducation</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+
+    </motion.div>
+    <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+
+I'm a skilled Machine Learning and AI Engineer with a background in Software Engineering. 
+Since 2020, I've worked on diverse projects, including recommendation systems and advanced NLP tasks. 
+My expertise in Generative AI allows me to leverage state-of-the-art algorithms to create innovative solutions. 
+I have experience in mobile app development, Java Servlet and JSP-based projects, location-based games, and blockchain integration. Proficient in Python, 
+I specialize in automation, data scraping, and web automation using Selenium and Beautiful Soup.
+Let's collaborate to shape the future of technology and bring your ideas to life!
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    
+    </>
+  )
+}
+
+export default About
---
+++ src/constants/index.js
@@ -0,0 +1,309 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+
+  import pythonIcon from '../assets/tech/python.png'; // Import the new icon
+  import tensorflowicon from '../assets/tech/tensorflow.png';
+  import Mlicon from '../assets/tech/ML.png';
+  import gai from '../assets/tech/GAI.png';
+  import NLP from '../assets/tech/nlp.png';
+  import pytorch from '../assets/tech/pytorch.png';
+  import sklearn from '../assets/tech/sklearn.png';
+  import selenium from '../assets/tech/selenium.png';
+  import BS from '../assets/tech/beautiful-soup.png';
+  import GI from '../assets/tech/Generative_AI.png';
+  import project from "../assets/tech/project1.png";
+  import java from "../assets/tech/java_based.png";
+  import chainlit from "../assets/tech/chainlit.png";
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Machine Learning",
+      icon: Mlicon,
+    },
+    {
+      title: "Web Developer",
+      icon: mobile,
+    },
+    {
+      title: "Generative AI",
+      icon: gai,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Natural language processing",
+      icon: NLP,
+    },
+ 
+
+  ];
+  
+  const technologies = [
+    {
+      name: "Python",
+      icon: pythonIcon,
+    },
+    {
+      name: "Tensoflow",
+      icon: tensorflowicon,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Pytorch",
+      icon: pytorch,
+    },
+    {
+      name: "Sklearn",
+      icon: sklearn,
+    },
+    {
+      name: "selenium",
+      icon: selenium,
+    },
+    {
+      name: "beautiful-soup",
+      icon: BS,
+    },
+    {
+      name: "Generative_AI",
+      icon: GI,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+ 
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Lead AI Engineer",
+      company_name: "Maktek – Remote",
+  
+      date: "08/2023-Present",
+      points: [
+        "Responsible for leading the development of conversational, generative, discriminative and Agentic AI solutions.",
+        "Expertise includes engaging with clients to secure projects and ensure their satisfaction.",
+        "Manages teams in the design, architecture, and implementation of AI projects.",
+        "Currently serving as a partner in product development with Maktek.",
+      ],
+    },
+
+    {
+      title: "Data Intelligent Product Developer",
+      company_name: "Bizlytix-Contract",
+      date: "04/2024-Present",
+      points: [
+        "Data Intelligent Product Developer at BliztlixTransforming raw data into actionable insights and developing intelligent algorithms to enhance product functionality and performance, driving business growth and improving customer experiences."
+        
+      ],
+    },
+    
+    {
+      title: "Lead LLM and ML Engineer",
+      company_name: "AIDevStudio",
+      date: "04/2024-Present",
+      points: [
+        "Lead LLM and ML Engineer at AI Dev Studio Leading AI and NLP projects, developing healthcare conversational solutions, and building advanced Retrieval-Augmented Generation systems for accurate and relevant responses."
+        
+      ],
+    },
+
+    {
+      title: "Machine Learning Eng/Researcher",
+      company_name: "COMSATS University Islamabad Abbottabad Campus",
+
+      date: "10/2020-Present",
+      points: [
+        "In my research role, I specialized in harnessing the power of Large Language Models for advanced Natural Language Processing.",
+        "Played a key role in extracting implicit and explicit aspects, conducting Sequence Classification, and Token Classification.",
+        "My contributions spanned Data Collection, creating Data Annotation rules, curating data, and implementing a custom scoring mechanism.",
+        "Notably, we pioneered a bias-free approach for Food Named Entity Recognition and developed a Food Ontology in Restaurant Domain, surpassing conventional systems in every aspect.",
+        "In addition to development, our secondary focus includes crafting Technical Write-ups for the developed product modules.",
+        "We are committed to ongoing research and system updates, adapting to the Retrieval Augmented Generation for better and more fluent responses.",
+        "Our next step involves implementing the Lang Chain framework to elevate recommendations using Large Language Models."
+
+      ],
+    },
+    {
+      title: "Cofounder & CTO",
+      company_name: "Hsieh – Onsite",
+      date: "10/2023-Present",
+      points: [
+        "Startup located in Plan-9 Arfa Kareem Tower. My responsibilities to lead research and development efforts furthermore refining Large Language Models to improve Sales and Customer Support.",
+        "I also focus on Generative AI for Data Augmentation and Annotation, Target Audience identification through Customer Segmentation based on Conversation Analysis, AI-based Customer and Agents Profiling, Voice Calls Analysis, Topic Modeling, Summarization, Speakers Digitization, Content Moderation, and ABSA etc.Lead AI."
+      ],
+    },
+    {
+      title: "Deep Learning Engineer",
+      company_name: "DevEntia Tech – Hybrid",
+      date: "09/2023-Present",
+      points: [
+        "Developing end-to-end Machine Learning/Deep Learning project including Natural Language Processing, Discriminative and Predictive AI, Generative AI, fine-tuning Language Models, utilization of OpenCV, proficiency in Prompt Engineering and Tuning, Retrieval Augmentation Generation (RAG) Applications, Vector Databases, Conversational AI and Chatbots, State of the Art Transformers etc..",
+       
+      ],
+    },
+    {
+      title: "Freelancing",
+      company_name: "Fiverr – Remote",
+      date: "07/2020-Present",
+      points: [
+        "Developing end-to-end Machine Learning/Deep Learning project including Natural Language Processing, Discriminative and Predictive AI, Generative AI, fine-tuning Language Models, utilization of OpenCV, proficiency in Prompt Engineering and Tuning, Retrieval Augmentation Generation (RAG) Applications, Vector Databases, Conversational AI and Chatbots, State of the Art Transformers etc..",
+       
+      ],
+    },
+    
+
+  ];
+
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Recommended. He is one of the best workers here. Always good work",
+      name: "joseomar18",
+      designation: "Fiverr Client",
+
+      image: "https://randomuser.me/api/portraits/men/31.jpg",
+    },
+    {
+      testimonial:
+        "Very fast replier. Very trust worthy. Very talented. Very hard worker i am very happy with the work given!",
+      name: "ajaybains123",
+      designation: "Fiverr Client",
+      image: "https://randomuser.me/api/portraits/men/85.jpg",
+    },
+    {
+      testimonial:
+        "Perfect work, completed.",
+      name:"alikhreis466",
+      designation: "Fiverr Client",
+      image: "https://randomuser.me/api/portraits/men/35.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "TradingView Indicators Chatbot",
+      description:
+        "This project focuses on developing a sophisticated chatbot capable of understanding user instructions to generate and write TradingView indicators. By leveraging retrieval-augmented generation (RAG) techniques, the chatbot can interact with users, providing insightful and accurate indicator scripts. The chatbot is trained on a comprehensive dataset of TradingView indicators, ensuring it is well-versed in various technical analysis tools and methods.",
+      tags: [
+        {
+          name: "RAG model ",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "dataset of TradingView indicator",
+          color: "green-text-gradient",
+        },
+        {
+          name: "chatchain",
+          color: "pink-text-gradient",
+        },
+      ],
+
+      image: project,
+      source_code_link: "https://github.com/AliHaider0343/Pine-Script-Code-Generator-RAG-/tree/main?tab=readme-ov-file",
+    },
+    {
+      name: "Welcome to CarRental ChatBot!",
+      description:
+      "The CarRental ChatBot is a conversational AI solution designed to streamline and enhance the car rental experience. This chatbot leverages advanced language models and cutting-edge technologies to provide customers with a user-friendly interface for renting vehicles, getting information about available cars, and receiving support for their car rental needs.",
+      tags: [
+        {
+          name: "Chainlit",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "OpenAI",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Langchain",
+          color: "pink-text-gradient",
+        },
+       
+      ],
+      image: chainlit,
+      source_code_link: "https://github.com/AliHaider0343/Pet-Toy-Recommendation-ChatBot/tree/main",
+    },
+
+    {
+      name: "Configurable-Data-Annotation-Tool-",
+      description:
+        "ConAno is an innovative NLP-assisted annotation tool designed to save time and resources while maintaining precision. It's tailored for tasks like sentiment analysis and aspect-based sentiment analysis, enhancing annotation efficiency and quality.",
+      tags: [
+        {
+          name: "NLP",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "sentiment analysis",
+          color: "green-text-gradient",
+        },
+        {
+          name: "annotation",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: java,
+      source_code_link: "https://github.com/AliHaider0343/Configurable-Data-Annotation-Tool-?tab=readme-ov-file",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
